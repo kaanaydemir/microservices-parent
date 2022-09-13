f2f1d363f03fc46d19670283ca6707be621e469f
kaan.aydemir.iu@gmail.com <PODQ29PIKk>
2022-09-13 19:20:04 +0300

    CloudConfigServer added

---
+++ cloud-config-server/src/test/java/com/kaanaydemir/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaanaydemir.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
