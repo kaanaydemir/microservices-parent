235ed0686e4c761d8717f8aeaceaa0ab428be007
kaan.aydemir.iu@gmail.com <PODQ29PIKk>
2022-09-14 22:29:17 +0300

    ELK added for logging

--- product-service/src/main/java/com/kaanaydemir/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/kaanaydemir/productservice/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.kaanaydemir.productservice.dto.ProductRequest;
 import com.kaanaydemir.productservice.dto.ProductResponse;
 import com.kaanaydemir.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/api/product")
+@Slf4j
 public class ProductController {
 
     private final ProductService productService;
@@ -20,6 +22,7 @@ public class ProductController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
+        log.info("ProductController.createProduct() is called");
         productService.createProduct (productRequest);
     }
 
